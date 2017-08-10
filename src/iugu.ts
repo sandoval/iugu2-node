9fa8d9bbaf845cbd0641b7ac66b4334fd8167a06
shirayukikitsune <shirayukikitsune@gmail.com>
2017-08-10 16:08:47 -0300

    fix linting

--- src/iugu.ts
+++ src/iugu.ts
@@ -1,6 +1,6 @@
 /**
  * The class for interaction with IUGU.
- * 
+ *
  * Should be constructed passing the API token as parameter (see https://dev.iugu.com/v1.0/reference#autentica%C3%A7%C3%A3o)
  */
 export class Iugu {
@@ -12,7 +12,7 @@ export class Iugu {
 
     /**
      * Initializes the API interaction instance
-     * 
+     *
      * @param apiToken the API token created for your account
      */
     constructor(apiToken: string) {

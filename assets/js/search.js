806a4ceeeb61b2ca58ff2aed51fad63d25a2f4c6
Travis CI <>
2017-08-15 21:22:41 +0000

    Deploy to GitHub Pages: dc5328b4a55de0f593112695e9bc5319a7a4493a

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+﻿var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"1":"External module","65536":"Type literal"},"rows":[{"id":0,"kind":65536,"name":"__type","url":"interfaces/_http_request_.response.html#status.__type","classes":"tsd-kind-type-literal tsd-parent-kind-property tsd-is-not-exported","parent":"\"http/request\".Response.status"},{"id":1,"kind":1,"name":"\"index\"","url":"modules/_index_.html","classes":"tsd-kind-external-module"}]};

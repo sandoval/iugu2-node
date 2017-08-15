fdfe01d5858dfef4d4194e580c511ab606be61ca
shirayukikitsune <shirayukikitsune@gmail.com>
2017-08-15 18:02:25 -0300

    fix typedoc

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -27,7 +27,7 @@ git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
 # Clean out existing contents
-rm -rf out/**/* || exit 0
+rm -rf out/* out/.* || exit 0
 
 # Run our compile script
 docGen
